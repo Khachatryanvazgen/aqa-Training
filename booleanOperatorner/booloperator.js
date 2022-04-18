5e8ba55d4c2a072654c886158339d802d951d3af
Khachatryanvazgen <vazgenkhachatryan6@gmail.com>
2022-04-18 20:12:12 +0400

    boolOperatorner

---
+++ booleanOperatorner/booloperator.js
@@ -0,0 +1,31 @@
+                                                                                //BOOLIAN OPERATORNER//
+
+                //EV Operator//
+
+/* let userResponce = +prompt("how old are you?");
+
+if(userResponce > 18 && userResponce < 60) {
+alert("dzec")
+}else{
+alert("yeah")
+} */
+
+                //KAM OPERATOR//
+
+/* let userResponce = +prompt("how old are you?");
+
+if(userResponce > 18 || userResponce < 10) {
+alert("dzec")
+}else{
+alert("yeah")
+} */
+
+                //VOCH OPERATOR//
+
+let userResponce = +prompt("how old are you?");
+
+if (userResponce > 18 && !(userResponce < 10)) {
+  alert("dzec");
+} else {
+  alert("yeah");
+}
